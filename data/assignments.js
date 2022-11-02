28bee4c05bd73822a5e8ccd9aa8c2ae1cde4dd2a
planit <planit@tanjeffreyz.github.io>
2022-11-02 05:24:43 +0000

    Updated assignments

---
+++ data/assignments.js
@@ -0,0 +1,393 @@
+assignments = {
+  "CS 61B": [
+    {
+      "title": "Lab01",
+      "course": "CS 61B",
+      "dueDate": "2022-09-02T23:59:00",
+      "link": "https://www.gradescope.com/courses/420508/assignments/2181782/submissions/131029423",
+      "submitted": true
+    },
+    {
+      "title": "Lab02",
+      "course": "CS 61B",
+      "dueDate": "2022-09-02T23:59:00",
+      "link": "https://www.gradescope.com/courses/420508/assignments/2191171/submissions/131628661",
+      "submitted": true
+    },
+    {
+      "title": "Lab03",
+      "course": "CS 61B",
+      "dueDate": "2022-09-09T23:59:00",
+      "link": "https://www.gradescope.com/courses/420508/assignments/2195062/submissions/132485822",
+      "submitted": true
+    },
+    {
+      "title": "Project 0: 2048",
+      "course": "CS 61B",
+      "dueDate": "2022-09-02T23:59:00",
+      "link": "https://www.gradescope.com/courses/420508/assignments/2195550/submissions/131535778",
+      "submitted": true
+    },
+    {
+      "title": "Homework 1",
+      "course": "CS 61B",
+      "dueDate": "2022-09-02T23:59:00",
+      "link": "https://www.gradescope.com/courses/420508/assignments/2212243/submissions/131629148",
+      "submitted": true
+    },
+    {
+      "title": "Lab04",
+      "course": "CS 61B",
+      "dueDate": "2022-09-16T23:59:00",
+      "link": "https://www.gradescope.com/courses/420508/assignments/2225909/submissions/134150731",
+      "submitted": true
+    },
+    {
+      "title": "Lab04A",
+      "course": "CS 61B",
+      "dueDate": "2022-09-16T23:59:00",
+      "link": "https://www.gradescope.com/courses/420508/assignments/2228063/submissions/134149857",
+      "submitted": true
+    },
+    {
+      "title": "Lab04B",
+      "course": "CS 61B",
+      "dueDate": "2022-09-16T23:59:00",
+      "link": "https://www.gradescope.com/courses/420508/assignments/2228065/submissions/134150308",
+      "submitted": true
+    },
+    {
+      "title": "[UNGRADED] Project 1a: LinkedListDeque",
+      "course": "CS 61B",
+      "dueDate": "2022-09-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/420508/assignments/2230372/submissions/135411506",
+      "submitted": true
+    },
+    {
+      "title": "[UNGRADED] Project 1b: ArrayDeque",
+      "course": "CS 61B",
+      "dueDate": "2022-09-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/420508/assignments/2230384/submissions/135410535",
+      "submitted": true
+    },
+    {
+      "title": "Project 1: Deques",
+      "course": "CS 61B",
+      "dueDate": "2022-09-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/420508/assignments/2230430/submissions/135487278",
+      "submitted": true
+    },
+    {
+      "title": "Lab06",
+      "course": "CS 61B",
+      "dueDate": "2022-09-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/420508/assignments/2248219/submissions/137279871",
+      "submitted": true
+    },
+    {
+      "title": "Homework 2",
+      "course": "CS 61B",
+      "dueDate": "2022-10-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/420508/assignments/2278511/submissions/138240394",
+      "submitted": true
+    },
+    {
+      "title": "Lab07",
+      "course": "CS 61B",
+      "dueDate": "2022-10-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/420508/assignments/2290540/submissions/138698790",
+      "submitted": true
+    },
+    {
+      "title": "Project 2a: Ngordnet (NGrams)",
+      "course": "CS 61B",
+      "dueDate": "2022-10-12T23:59:00",
+      "link": "https://www.gradescope.com/courses/420508/assignments/2312957/submissions/139295330",
+      "submitted": true
+    },
+    {
+      "title": "Lab08",
+      "course": "CS 61B",
+      "dueDate": "2022-10-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/420508/assignments/2333095/submissions/140096704",
+      "submitted": true
+    },
+    {
+      "title": "Homework 3: Midterm 2 Conceptual Review",
+      "course": "CS 61B",
+      "dueDate": "2022-10-18T23:59:00",
+      "link": "https://www.gradescope.com/courses/420508/assignments/2334423/submissions/141588552",
+      "submitted": true
+    },
+    {
+      "title": "Project 2b: Ngordnet (WordNet)",
+      "course": "CS 61B",
+      "dueDate": "2022-11-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/420508/assignments/2375648/submissions/144728115",
+      "submitted": true
+    },
+    {
+      "title": "Homework 3: Midterm 2 Conceptual Review [47B Only] ",
+      "course": "CS 61B",
+      "dueDate": "2022-12-09T23:59:00",
+      "link": "https://www.gradescope.com/courses/420508/assignments/2383165/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Lab11",
+      "course": "CS 61B",
+      "dueDate": "2022-11-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/420508",
+      "submitted": false
+    }
+  ],
+  "Data 88E": [
+    {
+      "title": "Lab 1",
+      "course": "Data 88E",
+      "dueDate": "2022-09-12T23:59:00",
+      "link": "https://www.gradescope.com/courses/419567/assignments/2199609/submissions/133290619",
+      "submitted": true
+    },
+    {
+      "title": "Project 1",
+      "course": "Data 88E",
+      "dueDate": "2022-09-26T23:59:00",
+      "link": "https://www.gradescope.com/courses/419567/assignments/2203389/submissions/136437989",
+      "submitted": true
+    },
+    {
+      "title": "Attendance 1",
+      "course": "Data 88E",
+      "dueDate": "2022-09-06T15:45:00",
+      "link": "https://www.gradescope.com/courses/419567/assignments/2231015/submissions/132195125",
+      "submitted": true
+    },
+    {
+      "title": "Lab 2",
+      "course": "Data 88E",
+      "dueDate": "2022-09-19T23:59:00",
+      "link": "https://www.gradescope.com/courses/419567/assignments/2247355/submissions/134929951",
+      "submitted": true
+    },
+    {
+      "title": "Attendance 2",
+      "course": "Data 88E",
+      "dueDate": "2022-09-13T15:15:00",
+      "link": "https://www.gradescope.com/courses/419567/assignments/2251575/submissions/133497380",
+      "submitted": true
+    },
+    {
+      "title": "Lab 3",
+      "course": "Data 88E",
+      "dueDate": "2022-09-26T23:59:00",
+      "link": "https://www.gradescope.com/courses/419567/assignments/2269233/submissions/135982151",
+      "submitted": true
+    },
+    {
+      "title": "Attendance 3",
+      "course": "Data 88E",
+      "dueDate": "2022-09-20T15:45:00",
+      "link": "https://www.gradescope.com/courses/419567/assignments/2274533/submissions/135083321",
+      "submitted": true
+    },
+    {
+      "title": "Lab 4",
+      "course": "Data 88E",
+      "dueDate": "2022-10-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/419567/assignments/2288545/submissions/138153068",
+      "submitted": true
+    },
+    {
+      "title": "Project 2",
+      "course": "Data 88E",
+      "dueDate": "2022-10-17T23:59:00",
+      "link": "https://www.gradescope.com/courses/419567/assignments/2295471/submissions/141415541",
+      "submitted": true
+    },
+    {
+      "title": "Attendance 4",
+      "course": "Data 88E",
+      "dueDate": "2022-09-27T15:30:00",
+      "link": "https://www.gradescope.com/courses/419567/assignments/2296897/submissions/136701935",
+      "submitted": true
+    },
+    {
+      "title": "Attendance 5",
+      "course": "Data 88E",
+      "dueDate": "2022-10-04T16:15:00",
+      "link": "https://www.gradescope.com/courses/419567/assignments/2321266/submissions/138418524",
+      "submitted": true
+    },
+    {
+      "title": "Lab 5",
+      "course": "Data 88E",
+      "dueDate": "2022-10-10T23:59:00",
+      "link": "https://www.gradescope.com/courses/419567/assignments/2323353/submissions/139615016",
+      "submitted": true
+    },
+    {
+      "title": "Lab 6",
+      "course": "Data 88E",
+      "dueDate": "2022-10-17T23:59:00",
+      "link": "https://www.gradescope.com/courses/419567/assignments/2335744/submissions/141191868",
+      "submitted": true
+    },
+    {
+      "title": "Attendance 6",
+      "course": "Data 88E",
+      "dueDate": "2022-10-11T16:00:00",
+      "link": "https://www.gradescope.com/courses/419567/assignments/2335785/submissions/140110706",
+      "submitted": true
+    },
+    {
+      "title": "Project 3",
+      "course": "Data 88E",
+      "dueDate": "2022-11-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/419567",
+      "submitted": false
+    },
+    {
+      "title": "Attendance 7",
+      "course": "Data 88E",
+      "dueDate": "2022-10-18T16:15:00",
+      "link": "https://www.gradescope.com/courses/419567/assignments/2360280/submissions/141868848",
+      "submitted": true
+    },
+    {
+      "title": "Lab 7",
+      "course": "Data 88E",
+      "dueDate": "2022-10-24T23:59:00",
+      "link": "https://www.gradescope.com/courses/419567/assignments/2373679/submissions/143609203",
+      "submitted": true
+    },
+    {
+      "title": "Lab 8",
+      "course": "Data 88E",
+      "dueDate": "2022-10-31T23:59:00",
+      "link": "https://www.gradescope.com/courses/419567/assignments/2380732/submissions/145345188",
+      "submitted": true
+    },
+    {
+      "title": "Attendance 8",
+      "course": "Data 88E",
+      "dueDate": "2022-10-25T16:00:00",
+      "link": "https://www.gradescope.com/courses/419567/assignments/2383583/submissions/143776252",
+      "submitted": true
+    },
+    {
+      "title": "Lab 9",
+      "course": "Data 88E",
+      "dueDate": "2022-11-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/419567",
+      "submitted": false
+    },
+    {
+      "title": "Attendance 9",
+      "course": "Data 88E",
+      "dueDate": "2022-11-01T16:15:00",
+      "link": "https://www.gradescope.com/courses/419567/assignments/2402988/submissions/145630084",
+      "submitted": true
+    }
+  ],
+  "MATH 1B": [
+    {
+      "title": "Homework 1",
+      "course": "MATH 1B",
+      "dueDate": "2022-08-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202136/submissions/131030615",
+      "submitted": true
+    },
+    {
+      "title": "Homework 2",
+      "course": "MATH 1B",
+      "dueDate": "2022-09-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202150/submissions/131903295",
+      "submitted": true
+    },
+    {
+      "title": "Homework 3",
+      "course": "MATH 1B",
+      "dueDate": "2022-09-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202151/submissions/133352397",
+      "submitted": true
+    },
+    {
+      "title": "Homework 4",
+      "course": "MATH 1B",
+      "dueDate": "2022-09-22T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202152/submissions/135756845",
+      "submitted": true
+    },
+    {
+      "title": "Homework 5",
+      "course": "MATH 1B",
+      "dueDate": "2022-09-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202153/submissions/136151742",
+      "submitted": true
+    },
+    {
+      "title": "Homework 6",
+      "course": "MATH 1B",
+      "dueDate": "2022-10-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202154/submissions/138151911",
+      "submitted": true
+    },
+    {
+      "title": "Homework 7",
+      "course": "MATH 1B",
+      "dueDate": "2022-10-11T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202155/submissions/139503937",
+      "submitted": true
+    },
+    {
+      "title": "Homework 8",
+      "course": "MATH 1B",
+      "dueDate": "2022-10-18T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202156/submissions/141996649",
+      "submitted": true
+    },
+    {
+      "title": "Homework 9",
+      "course": "MATH 1B",
+      "dueDate": "2022-10-28T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202157/submissions/143251582",
+      "submitted": true
+    },
+    {
+      "title": "Homework 10",
+      "course": "MATH 1B",
+      "dueDate": "2022-11-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202158/submissions/145517264",
+      "submitted": true
+    },
+    {
+      "title": "Project 1",
+      "course": "MATH 1B",
+      "dueDate": "2022-09-09T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202199/submissions/132408388",
+      "submitted": true
+    },
+    {
+      "title": "Project 2",
+      "course": "MATH 1B",
+      "dueDate": "2022-09-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202218/submissions/137697555",
+      "submitted": true
+    },
+    {
+      "title": "Project 3",
+      "course": "MATH 1B",
+      "dueDate": "2022-10-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202219/submissions/141129729",
+      "submitted": true
+    },
+    {
+      "title": "Project 4",
+      "course": "MATH 1B",
+      "dueDate": "2022-11-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777",
+      "submitted": false
+    }
+  ]
+};
