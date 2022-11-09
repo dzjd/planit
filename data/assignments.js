a6358277e1794dd2117e1b244905d94529799ee5
planit <planit@tanjeffreyz.github.io>
2022-11-09 02:49:33 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -290,10 +290,17 @@ assignments = {
       "submitted": true
     },
     {
+      "title": "Lab 10",
+      "course": "Data 88E",
+      "dueDate": "2022-11-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/419567",
+      "submitted": false
+    },
+    {
       "title": "Attendance 10 - Online",
       "course": "Data 88E",
       "dueDate": "2022-11-08T16:00:00",
-      "link": "https://www.gradescope.com/courses/419567/assignments/2416098/submissions/new",
+      "link": "https://www.gradescope.com/courses/419567",
       "submitted": false
     }
   ],
