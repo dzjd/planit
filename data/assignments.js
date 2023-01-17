fba38037548c66b99086dabbf472ae2dc3ab2292
planit <planit@tanjeffreyz.github.io>
2023-01-17 05:37:32 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -3,7 +3,7 @@ assignments = {
     {
       "title": "Mini-Vitamin 0A",
       "course": "CS 70",
-      "dueDate": "2023-01-19T11:00:00",
+      "dueDate": "2023-01-19T12:00:00",
       "link": "https://www.gradescope.com/courses/494477/assignments/2578052/submissions/new",
       "submitted": false
     }
