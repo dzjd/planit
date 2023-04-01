a6ed6fb389cb9edf1f5886a841a168fc23607282
planit <planit@tanjeffreyz.github.io>
2023-04-01 08:37:32 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -265,6 +265,13 @@ assignments = {
       "dueDate": "2023-03-25T16:00:00",
       "link": "https://www.gradescope.com/courses/494477",
       "submitted": false
+    },
+    {
+      "title": "April Fools Midterm 2",
+      "course": "CS 70",
+      "dueDate": "2023-04-09T00:00:00",
+      "link": "https://www.gradescope.com/courses/494477",
+      "submitted": false
     }
   ],
   "MATH 54": [
