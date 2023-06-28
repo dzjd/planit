467214cef7f7de0cfa3d4cb03afa8b313d401a5f
planit <planit@tanjeffreyz.github.io>
2023-06-28 10:40:28 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -25,8 +25,8 @@ assignments = {
       "title": "Lab 2",
       "course": "CS 61C",
       "dueDate": "2023-06-29T23:59:00",
-      "link": "https://www.gradescope.com/courses/549220",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/549220/assignments/2956390/submissions/184217244",
+      "submitted": true
     }
   ]
 };
