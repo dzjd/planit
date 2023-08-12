92cbfc719f2446bf354ceb684af597a37cadcebb
planit <planit@tanjeffreyz.github.io>
2023-08-12 19:37:26 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,1 +1,116 @@
-assignments = {};
+assignments = {
+  "CS 61C": [
+    {
+      "title": "Lab 0",
+      "course": "CS 61C",
+      "dueDate": "2023-06-26T23:59:00",
+      "link": "https://www.gradescope.com/courses/549220/assignments/2950861/submissions/183970740",
+      "submitted": true
+    },
+    {
+      "title": "Project 1",
+      "course": "CS 61C",
+      "dueDate": "2023-06-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/549220/assignments/2956058/submissions/184294078",
+      "submitted": true
+    },
+    {
+      "title": "Lab 1",
+      "course": "CS 61C",
+      "dueDate": "2023-06-29T23:59:00",
+      "link": "https://www.gradescope.com/courses/549220/assignments/2956388/submissions/184182181",
+      "submitted": true
+    },
+    {
+      "title": "Lab 2",
+      "course": "CS 61C",
+      "dueDate": "2023-06-29T23:59:00",
+      "link": "https://www.gradescope.com/courses/549220/assignments/2956390/submissions/184217244",
+      "submitted": true
+    },
+    {
+      "title": "Project 2A",
+      "course": "CS 61C",
+      "dueDate": "2023-07-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/549220/assignments/2964171/submissions/184432357",
+      "submitted": true
+    },
+    {
+      "title": "Project 2B",
+      "course": "CS 61C",
+      "dueDate": "2023-07-18T23:59:00",
+      "link": "https://www.gradescope.com/courses/549220/assignments/2964172/submissions/184712193",
+      "submitted": true
+    },
+    {
+      "title": "Lab 3",
+      "course": "CS 61C",
+      "dueDate": "2023-07-11T23:59:00",
+      "link": "https://www.gradescope.com/courses/549220/assignments/2964299/submissions/184424959",
+      "submitted": true
+    },
+    {
+      "title": "Lab 4",
+      "course": "CS 61C",
+      "dueDate": "2023-07-11T23:59:00",
+      "link": "https://www.gradescope.com/courses/549220/assignments/2964300/submissions/184509387",
+      "submitted": true
+    },
+    {
+      "title": "Lab 5",
+      "course": "CS 61C",
+      "dueDate": "2023-07-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/549220/assignments/2979310/submissions/184966049",
+      "submitted": true
+    },
+    {
+      "title": "Lab 6",
+      "course": "CS 61C",
+      "dueDate": "2023-07-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/549220/assignments/2979311/submissions/184976047",
+      "submitted": true
+    },
+    {
+      "title": "Project 3A",
+      "course": "CS 61C",
+      "dueDate": "2023-07-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/549220/assignments/2982583/submissions/185132439",
+      "submitted": true
+    },
+    {
+      "title": "Project 3B",
+      "course": "CS 61C",
+      "dueDate": "2023-08-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/549220/assignments/2982584/submissions/185256432",
+      "submitted": true
+    },
+    {
+      "title": "Project 4",
+      "course": "CS 61C",
+      "dueDate": "2023-08-08T23:59:00",
+      "link": "https://www.gradescope.com/courses/549220/assignments/2983995/submissions/186062515",
+      "submitted": true
+    },
+    {
+      "title": "Lab 7",
+      "course": "CS 61C",
+      "dueDate": "2023-07-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/549220/assignments/2986591/submissions/185389636",
+      "submitted": true
+    },
+    {
+      "title": "Lab 8",
+      "course": "CS 61C",
+      "dueDate": "2023-07-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/549220/assignments/2986592/submissions/185396950",
+      "submitted": true
+    },
+    {
+      "title": "Lab 9",
+      "course": "CS 61C",
+      "dueDate": "2023-08-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/549220/assignments/2987442/submissions/185566559",
+      "submitted": true
+    }
+  ]
+};
