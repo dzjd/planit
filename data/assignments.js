70c3d6f60e54b66e900c2c2cc3a80d3bd3c66db7
planit <planit@tanjeffreyz.github.io>
2023-08-23 19:37:38 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,5 +1,20 @@
 assignments = {
   "CS 162": [],
-  "CS 186": [],
+  "CS 186": [
+    {
+      "title": "[REQUIRED] Project 0 Debugging Exercise",
+      "course": "CS 186",
+      "dueDate": "2023-08-28T23:59:00",
+      "link": "https://www.gradescope.com/courses/580455/assignments/3130236/submissions/187950690",
+      "submitted": true
+    },
+    {
+      "title": "[REQUIRED] Project 0",
+      "course": "CS 186",
+      "dueDate": "2023-08-28T23:59:00",
+      "link": "https://www.gradescope.com/courses/580455/assignments/3130238/submissions/187950564",
+      "submitted": true
+    }
+  ],
   "CS188": []
 };
