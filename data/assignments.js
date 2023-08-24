9f1bf014f14a077632387bcce985495f475a95ed
planit <planit@tanjeffreyz.github.io>
2023-08-24 01:29:59 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -14,6 +14,13 @@ assignments = {
       "dueDate": "2023-08-28T23:59:00",
       "link": "https://www.gradescope.com/courses/580455/assignments/3130238/submissions/187950564",
       "submitted": true
+    },
+    {
+      "title": "[GRADED] Vitamin 4: Buffer Management and Relational Algebra",
+      "course": "CS 186",
+      "dueDate": "2023-02-17T23:59:00",
+      "link": "https://www.gradescope.com/courses/580455",
+      "submitted": false
     }
   ],
   "CS188": []
