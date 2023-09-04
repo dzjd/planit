4b5248be39d2b10580cc65e80040f587ff150132
planit <planit@tanjeffreyz.github.io>
2023-09-04 20:37:35 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -48,8 +48,8 @@ assignments = {
       "title": "[GRADED] Vitamin 2: Disks, Buffers, Files",
       "course": "CS 186",
       "dueDate": "2023-09-08T23:59:00",
-      "link": "https://www.gradescope.com/courses/580455/assignments/3156861/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/580455/assignments/3156861/submissions/190216684",
+      "submitted": true
     },
     {
       "title": "[GRADED] Project 1",
