5fbfd9b18762dac6956ae9022f1c7b4a654cc4b2
planit <planit@tanjeffreyz.github.io>
2023-10-13 18:38:59 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -132,8 +132,8 @@ assignments = {
       "title": "[GRADED] Vitamin 6: Iterators, Joins, and Relational Algebra",
       "course": "CS 186",
       "dueDate": "2023-10-16T23:59:00",
-      "link": "https://www.gradescope.com/courses/580455/assignments/3156900/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/580455/assignments/3156900/submissions/202330010",
+      "submitted": true
     },
     {
       "title": "[GRADED] Vitamin 7: Query Optimization",
