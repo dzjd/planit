af85e80817e3ecf3f5e0260d6c682d42f56dc407
planit <planit@tanjeffreyz.github.io>
2023-10-14 22:37:20 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -24,8 +24,8 @@ assignments = {
     {
       "title": "Homework 3 Written (C only)",
       "course": "CS 162",
-      "dueDate": "2023-10-13T12:20:00",
-      "link": "https://www.gradescope.com/courses/565424",
+      "dueDate": "2023-10-23T12:20:00",
+      "link": "https://www.gradescope.com/courses/565424/assignments/3294798/submissions/new",
       "submitted": false
     },
     {
