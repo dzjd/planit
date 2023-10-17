8ae56ee7d4d26143f8c6a30542e5417d40328c21
planit <planit@tanjeffreyz.github.io>
2023-10-17 05:37:16 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -50,6 +50,13 @@ assignments = {
       "submitted": false
     },
     {
+      "title": "Project 1 Staff Solution Essay",
+      "course": "CS 162",
+      "dueDate": "2023-10-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/565424",
+      "submitted": false
+    },
+    {
       "title": "Project 2 Report",
       "course": "CS 162",
       "dueDate": "2023-11-08T23:59:00",
