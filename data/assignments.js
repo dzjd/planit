9b9a90f67759297cf96f18053eac53d03cb79b2c
planit <planit@tanjeffreyz.github.io>
2023-10-17 23:37:21 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -195,8 +195,8 @@ assignments = {
       "title": "[GRADED] Project 3 Part 2",
       "course": "CS 186",
       "dueDate": "2023-10-18T23:59:00",
-      "link": "https://www.gradescope.com/courses/580455",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/580455/assignments/3422894/submissions/203516204",
+      "submitted": true
     },
     {
       "title": "[PRACTICE] Vitamin 4: Buffer Management and Spatial Indexes",
