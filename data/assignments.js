040b518b07c71842a54c66def531f7bb151eec14
planit <planit@tanjeffreyz.github.io>
2023-10-23 15:37:30 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -150,6 +150,13 @@ assignments = {
       "submitted": true
     },
     {
+      "title": "[GRADED] Vitamin 8: Transactions and Concurrency",
+      "course": "CS 186",
+      "dueDate": "2023-10-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/580455/assignments/3156903/submissions/new",
+      "submitted": false
+    },
+    {
       "title": "[GRADED] Project 1",
       "course": "CS 186",
       "dueDate": "2023-09-08T23:59:00",
