750affe0b93a7df71216bbc0be475aacad765960
planit <planit@tanjeffreyz.github.io>
2023-10-29 18:37:24 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -407,8 +407,8 @@ assignments = {
       "title": "Homework 7 (Part 2)",
       "course": "CS188",
       "dueDate": "2023-10-31T23:59:00",
-      "link": "https://www.gradescope.com/courses/572452/assignments/3582896/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/572452/assignments/3582896/submissions/207376608",
+      "submitted": true
     }
   ]
 };
