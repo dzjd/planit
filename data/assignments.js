b44ca7fa38ebc56607718eb5375c821b5872d825
planit <planit@tanjeffreyz.github.io>
2023-10-30 15:37:28 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -157,6 +157,13 @@ assignments = {
       "submitted": true
     },
     {
+      "title": "[GRADED] Vitamin 10: Recovery",
+      "course": "CS 186",
+      "dueDate": "2023-11-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/580455/assignments/3156918/submissions/new",
+      "submitted": false
+    },
+    {
       "title": "[GRADED] Project 1",
       "course": "CS 186",
       "dueDate": "2023-09-08T23:59:00",
