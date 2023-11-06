16e1a947f5fcbfb74a6a9cff567c6a392db9031b
planit <planit@tanjeffreyz.github.io>
2023-11-06 16:41:51 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -164,6 +164,13 @@ assignments = {
       "submitted": false
     },
     {
+      "title": "[GRADED] Vitamin 10: Parallel Query Processing",
+      "course": "CS 186",
+      "dueDate": "2023-11-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/580455/assignments/3156919/submissions/new",
+      "submitted": false
+    },
+    {
       "title": "[GRADED] Project 1",
       "course": "CS 186",
       "dueDate": "2023-09-08T23:59:00",
