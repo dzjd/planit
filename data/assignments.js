d313b25d74501ebff74f6ab8d1dbbf78dc535a9c
planit <planit@tanjeffreyz.github.io>
2023-11-13 16:41:07 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -171,6 +171,13 @@ assignments = {
       "submitted": true
     },
     {
+      "title": "[GRADED] Vitamin 11: Distributed Transactions",
+      "course": "CS 186",
+      "dueDate": "2023-11-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/580455/assignments/3156920/submissions/new",
+      "submitted": false
+    },
+    {
       "title": "[GRADED] Project 1",
       "course": "CS 186",
       "dueDate": "2023-09-08T23:59:00",
