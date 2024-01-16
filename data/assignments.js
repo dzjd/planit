ead75c49a83ffe985181a18c385241934702d551
planit <planit@tanjeffreyz.github.io>
2024-01-16 18:38:34 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,26 +1,4 @@
 assignments = {
   "CS 161": [],
-  "Data 100": [
-    {
-      "title": "Syllabus Quiz",
-      "course": "Data 100",
-      "dueDate": "2024-08-31T23:59:00",
-      "link": "https://www.gradescope.com/courses/696886",
-      "submitted": false
-    },
-    {
-      "title": "Homework 1 Coding",
-      "course": "Data 100",
-      "dueDate": "2024-08-31T23:59:00",
-      "link": "https://www.gradescope.com/courses/696886",
-      "submitted": false
-    },
-    {
-      "title": "Homework 1 Written",
-      "course": "Data 100",
-      "dueDate": "2024-08-31T23:59:00",
-      "link": "https://www.gradescope.com/courses/696886",
-      "submitted": false
-    }
-  ]
+  "Data 100": []
 };
