e8494818966f578ac922a9167a233e259589e6cb
planit <planit@tanjeffreyz.github.io>
2024-01-19 21:37:26 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -3,6 +3,13 @@ assignments = {
   "CS 170": [],
   "Data 100": [
     {
+      "title": "Syllabus Quiz",
+      "course": "Data 100",
+      "dueDate": "2024-02-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/696886/assignments/3932755/submissions/new",
+      "submitted": false
+    },
+    {
       "title": "Lab 01",
       "course": "Data 100",
       "dueDate": "2024-01-23T23:59:00",
