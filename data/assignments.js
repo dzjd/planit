58030aea5019cbc64835f5a10134f586aedb6633
planit <planit@tanjeffreyz.github.io>
2024-01-19 22:37:30 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -13,8 +13,8 @@ assignments = {
       "title": "Lab 01",
       "course": "Data 100",
       "dueDate": "2024-01-23T23:59:00",
-      "link": "https://www.gradescope.com/courses/696886",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/696886/assignments/3932825/submissions/223355181",
+      "submitted": true
     },
     {
       "title": "Homework 1A Coding",
