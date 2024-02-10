f130b3d605e16f7866f8820a0ed592b2b2793c4e
planit <planit@tanjeffreyz.github.io>
2024-02-10 16:37:27 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -34,6 +34,13 @@ assignments = {
       "dueDate": "2024-02-09T23:59:00",
       "link": "https://www.gradescope.com/courses/708067/assignments/4014060/submissions/225553773",
       "submitted": true
+    },
+    {
+      "title": "Homework 3",
+      "course": "CS 161",
+      "dueDate": "2024-09-29T23:59:00",
+      "link": "https://www.gradescope.com/courses/708067",
+      "submitted": false
     }
   ],
   "CS 170": [
