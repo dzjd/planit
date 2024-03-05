79e9bdd8deae2e3fd0d320a380217deb8446ba5a
planit <planit@tanjeffreyz.github.io>
2024-03-05 19:37:17 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -148,6 +148,20 @@ assignments = {
       "dueDate": "2024-03-04T22:00:00",
       "link": "https://www.gradescope.com/courses/701957/assignments/4165642/submissions/236054568",
       "submitted": true
+    },
+    {
+      "title": "Homework 7 Coding Portion",
+      "course": "CS 170",
+      "dueDate": "2024-03-11T22:00:00",
+      "link": "https://www.gradescope.com/courses/701957",
+      "submitted": false
+    },
+    {
+      "title": "Homework 7 Written Portion",
+      "course": "CS 170",
+      "dueDate": "2024-03-11T22:00:00",
+      "link": "https://www.gradescope.com/courses/701957",
+      "submitted": false
     }
   ],
   "Data 100": [
