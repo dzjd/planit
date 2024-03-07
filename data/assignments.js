e4772039d83f42714399a2688a3d4efcf9deb06b
planit <planit@tanjeffreyz.github.io>
2024-03-07 22:37:31 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -90,6 +90,13 @@ assignments = {
       "dueDate": "2024-03-22T23:59:00",
       "link": "https://www.gradescope.com/courses/708067/assignments/4192957/submissions/new",
       "submitted": false
+    },
+    {
+      "title": "[Optional] Homework 5 Lab Autograder",
+      "course": "CS 161",
+      "dueDate": "2024-03-22T23:59:00",
+      "link": "https://www.gradescope.com/courses/708067",
+      "submitted": false
     }
   ],
   "CS 170": [
