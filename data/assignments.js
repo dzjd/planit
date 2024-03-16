fd04fb38cfec170036e63c1529de21069f867abe
planit <planit@tanjeffreyz.github.io>
2024-03-16 07:37:13 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -397,6 +397,27 @@ assignments = {
       "submitted": false
     },
     {
+      "title": "Project A2 Coding",
+      "course": "Data 100",
+      "dueDate": "2024-03-21T23:59:00",
+      "link": "https://www.gradescope.com/courses/696886",
+      "submitted": false
+    },
+    {
+      "title": "Project A2 Written",
+      "course": "Data 100",
+      "dueDate": "2024-03-21T23:59:00",
+      "link": "https://www.gradescope.com/courses/696886",
+      "submitted": false
+    },
+    {
+      "title": "Project A2 Test Set Predictions",
+      "course": "Data 100",
+      "dueDate": "2024-03-21T23:59:00",
+      "link": "https://www.gradescope.com/courses/696886",
+      "submitted": false
+    },
+    {
       "title": "Lab 08",
       "course": "Data 100",
       "dueDate": "2024-03-19T23:59:00",
