5a0582bf266cd1207ae9bb4ab531cc46540f4fb3
planit <planit@tanjeffreyz.github.io>
2024-03-18 09:38:30 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -358,7 +358,7 @@ assignments = {
       "title": "Grade Report",
       "course": "Data 100",
       "dueDate": "2024-05-18T00:00:00",
-      "link": "https://www.gradescope.com/courses/696886/assignments/4122047/submissions/238948779",
+      "link": "https://www.gradescope.com/courses/696886/assignments/4122047/submissions/239789421",
       "submitted": true
     },
     {
