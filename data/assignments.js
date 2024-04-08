be2f47ba970b8f6bf98c21beda7c705b1c3d8617
planit <planit@tanjeffreyz.github.io>
2024-04-08 06:37:59 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -281,7 +281,7 @@ assignments = {
       "title": "Lecture Attendance",
       "course": "Data 100",
       "dueDate": "2024-05-12T23:59:00",
-      "link": "https://www.gradescope.com/courses/696886/assignments/3972426/submissions/243524873",
+      "link": "https://www.gradescope.com/courses/696886/assignments/3972426/submissions/244868190",
       "submitted": true
     },
     {
