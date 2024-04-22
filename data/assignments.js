1e46ef998cd5110db0b9f71b826e97824592898b
planit <planit@tanjeffreyz.github.io>
2024-04-22 07:37:38 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -612,6 +612,20 @@ assignments = {
       "dueDate": "2024-05-05T23:59:00",
       "link": "https://www.gradescope.com/courses/696886/assignments/4365507/submissions/247255150",
       "submitted": true
+    },
+    {
+      "title": "[Data 200 Grad Project] First Draft",
+      "course": "Data 100",
+      "dueDate": "2024-04-26T23:59:00",
+      "link": "https://www.gradescope.com/courses/696886",
+      "submitted": false
+    },
+    {
+      "title": "[Data 200 Grad Project] First Draft (Coding)",
+      "course": "Data 100",
+      "dueDate": "2024-04-26T23:59:00",
+      "link": "https://www.gradescope.com/courses/696886",
+      "submitted": false
     }
   ],
   "IB 35AC": []
