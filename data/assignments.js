6979d29e5a13ab5ec9ebb1503a7c84a596e51f07
planit <planit@tanjeffreyz.github.io>
2024-08-14 21:37:41 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,11 +1,3 @@
 assignments = {
-  "CS 197 CPT ": [
-    {
-      "title": "Report",
-      "course": "CS 197 CPT ",
-      "dueDate": "2024-08-31T12:00:00",
-      "link": "https://www.gradescope.com/courses/808227",
-      "submitted": false
-    }
-  ]
+  "CS 168": []
 };
