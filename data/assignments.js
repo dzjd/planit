ee169f5f020662c008ad959ad04bca7d5bb654e8
planit <planit@tanjeffreyz.github.io>
2025-01-01 02:10:53 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -3,77 +3,77 @@ assignments = {
     {
       "title": "10.5 Iteration 1 (Individual) Retrospective",
       "course": "COMPSCI 169A FA24",
-      "dueDate": "2024-12-12T23:59:00",
+      "dueDate": "2025-12-12T23:59:00",
       "link": "https://www.gradescope.com/courses/903858/assignments/5229591/submissions/291197350",
       "submitted": true
     },
     {
       "title": "[Practice Final] Fall 2020 Final Exam",
       "course": "COMPSCI 169A FA24",
-      "dueDate": "2024-12-20T22:00:00",
+      "dueDate": "2025-12-20T22:00:00",
       "link": "https://www.gradescope.com/courses/903858",
       "submitted": false
     },
     {
       "title": "[Practice Final] Fall 2021 Final Exam",
       "course": "COMPSCI 169A FA24",
-      "dueDate": "2024-12-20T11:00:00",
+      "dueDate": "2025-12-20T11:00:00",
       "link": "https://www.gradescope.com/courses/903858",
       "submitted": false
     },
     {
       "title": "10.5: Iteration 1 Planning Meeting",
       "course": "COMPSCI 169A FA24",
-      "dueDate": "2024-11-11T23:59:00",
+      "dueDate": "2025-11-11T23:59:00",
       "link": "https://www.gradescope.com/courses/903858/assignments/5229607/submissions/288872575",
       "submitted": true
     },
     {
       "title": "10.5  Iteration 2: Planning Meeting",
       "course": "COMPSCI 169A FA24",
-      "dueDate": "2024-11-25T23:59:00",
+      "dueDate": "2025-11-25T23:59:00",
       "link": "https://www.gradescope.com/courses/903858/assignments/5229610/submissions/293603952",
       "submitted": true
     },
     {
       "title": "10.5: Iteration 2 Video Submission (Team)",
       "course": "COMPSCI 169A FA24",
-      "dueDate": "2024-12-06T23:59:00",
+      "dueDate": "2025-12-06T23:59:00",
       "link": "https://www.gradescope.com/courses/903858/assignments/5229611/submissions/297122500",
       "submitted": true
     },
     {
       "title": "10.5: Iteration 1 Video Submission",
       "course": "COMPSCI 169A FA24",
-      "dueDate": "2024-11-18T23:59:00",
+      "dueDate": "2025-11-18T23:59:00",
       "link": "https://www.gradescope.com/courses/903858/assignments/5229612/submissions/292334460",
       "submitted": true
     },
     {
       "title": "10.5 Iteration 1: Code Submission",
       "course": "COMPSCI 169A FA24",
-      "dueDate": "2024-11-18T23:59:00",
+      "dueDate": "2025-11-18T23:59:00",
       "link": "https://www.gradescope.com/courses/903858/assignments/5267466/submissions/293300916",
       "submitted": true
     },
     {
       "title": "10.5 Iteration 2 (Individual) Retrospective",
       "course": "COMPSCI 169A FA24",
-      "dueDate": "2024-12-06T23:59:00",
+      "dueDate": "2025-12-06T23:59:00",
       "link": "https://www.gradescope.com/courses/903858/assignments/5346433/submissions/297158975",
       "submitted": true
     },
     {
       "title": "10.5: Iteration 2 Code Submission (Team)",
       "course": "COMPSCI 169A FA24",
-      "dueDate": "2024-12-06T23:59:00",
+      "dueDate": "2025-12-06T23:59:00",
       "link": "https://www.gradescope.com/courses/903858/assignments/5365233/submissions/297118976",
       "submitted": true
     },
     {
       "title": "Submit Your Cheat Sheet",
       "course": "COMPSCI 169A FA24",
-      "dueDate": "2024-12-19T18:00:00",
+      "dueDate": "2025-12-19T18:00:00",
       "link": "https://www.gradescope.com/courses/903858/assignments/5411593/submissions/298241214",
       "submitted": true
     }
@@ -82,84 +82,84 @@ assignments = {
     {
       "title": "Project 1B: Traceroute Error Handling",
       "course": "CS 168",
-      "dueDate": "2024-09-20T23:59:00",
+      "dueDate": "2025-09-20T23:59:00",
       "link": "https://www.gradescope.com/courses/823258/assignments/4823891/submissions/265201189",
       "submitted": true
     },
     {
       "title": "Project 2: Routing",
       "course": "CS 168",
-      "dueDate": "2024-10-04T23:59:00",
+      "dueDate": "2025-10-04T23:59:00",
       "link": "https://www.gradescope.com/courses/823258/assignments/4823936/submissions/272632185",
       "submitted": true
     },
     {
       "title": "Project 3A: Transport Checkpoint",
       "course": "CS 168",
-      "dueDate": "2024-11-01T23:59:00",
+      "dueDate": "2025-11-01T23:59:00",
       "link": "https://www.gradescope.com/courses/823258/assignments/4824048/submissions/284690838",
       "submitted": true
     },
     {
       "title": "Project 3B: Transport",
       "course": "CS 168",
-      "dueDate": "2024-11-12T23:59:00",
+      "dueDate": "2025-11-12T23:59:00",
       "link": "https://www.gradescope.com/courses/823258/assignments/4824050/submissions/284894771",
       "submitted": true
     },
     {
       "title": "Project 1A: Basic Traceroute",
       "course": "CS 168",
-      "dueDate": "2024-09-10T23:59:00",
+      "dueDate": "2025-09-10T23:59:00",
       "link": "https://www.gradescope.com/courses/823258/assignments/4824053/submissions/263815688",
       "submitted": true
     },
     {
       "title": "HW1: Internet Architecture",
       "course": "CS 168",
-      "dueDate": "2024-09-30T23:59:00",
+      "dueDate": "2025-09-30T23:59:00",
       "link": "https://www.gradescope.com/courses/823258/assignments/4903817/submissions/272453088",
       "submitted": true
     },
     {
       "title": "[Ungraded] Lecture Attendance Checker",
       "course": "CS 168",
-      "dueDate": "2024-12-31T23:59:00",
+      "dueDate": "2025-12-31T23:59:00",
       "link": "https://www.gradescope.com/courses/823258",
       "submitted": false
     },
     {
       "title": "HW2: Routing",
       "course": "CS 168",
-      "dueDate": "2024-10-14T23:59:00",
+      "dueDate": "2025-10-14T23:59:00",
       "link": "https://www.gradescope.com/courses/823258/assignments/5112738/submissions/277313533",
       "submitted": true
     },
     {
       "title": "HW3: Transport",
       "course": "CS 168",
-      "dueDate": "2024-10-28T23:59:00",
+      "dueDate": "2025-10-28T23:59:00",
       "link": "https://www.gradescope.com/courses/823258/assignments/5190541/submissions/284657243",
       "submitted": true
     },
     {
       "title": "HW4: Applications, End-to-End",
       "course": "CS 168",
-      "dueDate": "2024-11-18T23:59:00",
+      "dueDate": "2025-11-18T23:59:00",
       "link": "https://www.gradescope.com/courses/823258/assignments/5309211/submissions/290481580",
       "submitted": true
     },
     {
       "title": "HW5: Datacenters",
       "course": "CS 168",
-      "dueDate": "2024-11-25T23:59:00",
+      "dueDate": "2025-11-25T23:59:00",
       "link": "https://www.gradescope.com/courses/823258/assignments/5359246/submissions/292392553",
       "submitted": true
     },
     {
       "title": "HW6: Special Topics",
       "course": "CS 168",
-      "dueDate": "2024-12-06T23:59:00",
+      "dueDate": "2025-12-06T23:59:00",
       "link": "https://www.gradescope.com/courses/823258/assignments/5392444/submissions/295420378",
       "submitted": true
     }
@@ -168,7 +168,7 @@ assignments = {
     {
       "title": "CPT Report.",
       "course": "CS 197 CPT",
-      "dueDate": "2024-12-31T00:00:00",
+      "dueDate": "2025-12-31T00:00:00",
       "link": "https://www.gradescope.com/courses/928139",
       "submitted": false
     }
