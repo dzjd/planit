1a803e3db70001157a5d598f76cd3378822620e6
planit <planit@tanjeffreyz.github.io>
2025-01-15 09:38:24 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,3 +1,18 @@
 assignments = {
-  "CS 189/289A": []
+  "CS 189/289A": [
+    {
+      "title": "CS289A Project Report",
+      "course": "CS 189/289A",
+      "dueDate": "2025-05-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/949051",
+      "submitted": false
+    },
+    {
+      "title": "CS289A Project Video",
+      "course": "CS 189/289A",
+      "dueDate": "2025-05-12T23:59:00",
+      "link": "https://www.gradescope.com/courses/949051",
+      "submitted": false
+    }
+  ]
 };
