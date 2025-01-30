59d1beedc67eaf180ab9c0b2888b9e3610d32c49
planit <planit@tanjeffreyz.github.io>
2025-01-30 01:54:04 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -34,13 +34,6 @@ assignments = {
       "dueDate": "2025-01-29T23:59:00",
       "link": "https://www.gradescope.com/courses/949051",
       "submitted": false
-    },
-    {
-      "title": "HW2 Write-Up",
-      "course": "CS 189/289A",
-      "dueDate": "2025-02-12T23:59:00",
-      "link": "https://www.gradescope.com/courses/949051",
-      "submitted": false
     }
   ]
 };
