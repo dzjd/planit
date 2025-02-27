242e65554f94400658ced5870847cc5eb84eb91b
planit <planit@tanjeffreyz.github.io>
2025-02-27 04:43:08 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -45,11 +45,25 @@ assignments = {
       "submitted": false
     },
     {
+      "title": "HW4 Code",
+      "course": "CS 189/289A",
+      "dueDate": "2025-03-12T23:59:00",
+      "link": "https://www.gradescope.com/courses/949051",
+      "submitted": false
+    },
+    {
       "title": "HW1 Code",
       "course": "CS 189/289A",
       "dueDate": "2025-01-29T23:59:00",
       "link": "https://www.gradescope.com/courses/949051/assignments/5576300/submissions/303641901",
       "submitted": true
+    },
+    {
+      "title": "HW4 Write-Up",
+      "course": "CS 189/289A",
+      "dueDate": "2025-03-12T23:59:00",
+      "link": "https://www.gradescope.com/courses/949051",
+      "submitted": false
     },
     {
       "title": "HW1 Write-Up",
