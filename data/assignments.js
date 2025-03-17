5598224248c0e5c82615ecf19ff9f77ef9ef7e2d
planit <planit@tanjeffreyz.github.io>
2025-03-17 05:41:06 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -18,8 +18,8 @@ assignments = {
       "title": "Programming Assignment 3: Web APIs and design mockups",
       "course": "CS 160/260A (Spring 2025)",
       "dueDate": "2025-03-16T23:59:00",
-      "link": "https://www.gradescope.com/courses/959556",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/959556/assignments/5865665/submissions/317786396",
+      "submitted": true
     }
   ],
   "CS 189/289A": [
